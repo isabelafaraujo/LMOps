79449a3ccca07df5219bc8ab8ff25be36cf8177b
Isabela Araújo <belaaj63@gmail.com>
2023-03-02 00:44:47 -0300

    fix file path

--- structured_prompting/fairseq-version/struprompting/tasks/__init__.py
+++ structured_prompting/fairseq-version/struprompting/tasks/__init__.py
@@ -1,7 +1,7 @@
 import argparse
 import importlib
 import os
-from fairseq.tasks import import_tasks
+from fairseq.fairseq.tasks import import_tasks
 
 tasks_dir = os.path.dirname(__file__)
 import_tasks(tasks_dir, "struprompting.tasks")
